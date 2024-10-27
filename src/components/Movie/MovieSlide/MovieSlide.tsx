9e3b54084246d55f15cae851d7bdd7f957452395
Moose <kimson1391@gmail.com>
2024-10-28 00:30:35 +0700

    Update more like this button in movie slide list

--- src/components/Movie/MovieSlide/MovieSlide.tsx
+++ src/components/Movie/MovieSlide/MovieSlide.tsx
@@ -16,8 +16,20 @@ export const MovieSlide = ({ title, type, category }: IMovieSlideProps) => {
         <h3 className='text-xl font-bold tracking-wider uppercase text-white'>{title}</h3>
       </div>
       <MovieList type={type} category={category} />
-      <Button className='w-full text-right mt-8 px-4'>
-        <Link to='/'>More like this</Link>
+      <Button className='w-full text-right mt-4 px-4'>
+        <Link to='/' className='flex items-center justify-end hover:text-mainOrange duration-main'>
+          <span>More like this</span>
+          <svg
+            className='h-4 ml-2'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+          >
+            <path stroke-linecap='round' stroke-linejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+          </svg>
+        </Link>
       </Button>
     </section>
   )
